bbdeb4c5c01427184b01712ea8c607d71ece62c5
Raymer <raymerdjpacheco1997@gmail.com>
2026-02-09 09:43:00 -0500

    Este es mi programa del parcial

---
+++ Programa para el parcial 1.py
@@ -0,0 +1,13 @@
+# *************************************
+# Desarrollado por Raymer Pacheco
+# *************************************
+# inicia desarollo de la funcion dada 
+# Función para contar el número de elementos en una lista. 
+def contar_elementos_lista(elementos):
+    # Contamos el número de elementos de la lista
+    return len(elementos)
+
+# Ejemplo de uso
+lista_ejemplo = [1, 2, 3, 4, 5]
+numero_de_elementos = contar_elementos_lista(lista_ejemplo)
+print("El número de elementos en la lista es:", numero_de_elementos)
